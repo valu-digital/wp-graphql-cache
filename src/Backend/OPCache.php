3dc5986feb0421215c6dd282c60979d462fdf892
Esa-Matti Suuronen <esa-matti@suuronen.org>
2020-05-06 19:09:24 +0300

    Add WIP OPCache backend

---
+++ src/Backend/OPCache.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WPGraphQL\Extensions\Cache\Backend;
+
+use WPGraphQL\Extensions\Cache\CachedValue;
+
+class OPCache extends FileSystem
+{
+    // XXX TODO
+}
