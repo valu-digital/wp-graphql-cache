8d8a58bbcb7adfa739801a63990e3c80bff79b5a
Esa-Matti Suuronen <esa-matti@suuronen.org>
2020-05-08 17:49:03 +0300

    Add full cache clear test

--- tests/wpunit/FieldCacheTest.php
+++ tests/wpunit/FieldCacheTest.php
@@ -106,6 +106,81 @@ class ExampleTest extends \Codeception\TestCase\WPTestCase
         );
     }
 
+    public function testFullCacheClear()
+    {
+        CacheManager::register_graphql_field_cache([
+            'zone' => 'ding_zone',
+            'query_name' => 'getPosts',
+            'field_name' => 'ding',
+        ]);
+
+        CacheManager::register_graphql_field_cache([
+            'zone' => 'dong_zone',
+            'query_name' => 'getPosts',
+            'field_name' => 'dong',
+        ]);
+
+        $ding_id = self::factory()->post->create([
+            'post_title' => 'Ding',
+        ]);
+
+        $dong_id = self::factory()->post->create([
+            'post_title' => 'Dong',
+        ]);
+
+        $query = '
+		query getPosts( $dingId: ID!, $dongId: ID! ) {
+            ding: post( id: $dingId, idType: DATABASE_ID ) {
+                title
+            }
+
+            dong: post( id: $dongId, idType: DATABASE_ID ) {
+                title
+            }
+ 		}
+		';
+
+        $actual = graphql([
+            'query' => $query,
+            'operationname' => 'getPosts',
+            'variables' => [
+                'dingId' => $ding_id,
+                'dongId' => $dong_id,
+            ],
+        ]);
+
+        // Reponds correctly on cache miss
+        $this->assertArrayNotHasKey('errors', $actual, print_r($actual, true));
+        $this->assertEquals('Ding', $actual['data']['ding']['title']);
+        $this->assertEquals('Dong', $actual['data']['dong']['title']);
+
+        wp_update_post([
+            'ID' => $ding_id,
+            'post_title' => 'Updated Ding',
+        ]);
+
+        wp_update_post([
+            'ID' => $dong_id,
+            'post_title' => 'Updated Dong',
+        ]);
+
+        // Clear both zones
+        CacheManager::clear();
+
+        $actual = graphql([
+            'query' => $query,
+            'operationname' => 'getPosts',
+            'variables' => [
+                'dingId' => $ding_id,
+                'dongId' => $dong_id,
+            ],
+        ]);
+        $this->assertArrayNotHasKey('errors', $actual, print_r($actual, true));
+
+        $this->assertEquals('Updated Ding', $actual['data']['ding']['title']);
+        $this->assertEquals('Updated Dong', $actual['data']['dong']['title']);
+    }
+
     public function testCanCacheMultipleFieldsWithZones()
     {
         CacheManager::register_graphql_field_cache([
