d8d2977de0f54b0fc7d373970af0f974c34f6dbb
Esa-Matti Suuronen <esa-matti@suuronen.org>
2020-05-08 17:10:35 +0300

    Fix autoloaders

--- plugin.php
+++ plugin.php
@@ -7,6 +7,18 @@
  * Version: 0.1.0
  */
 
-namespace WPGraphQL\Extensions\Cache;
+// To make this plugin work properly for both Composer users and non-composer
+// users we must detect whether the project is using a global autoloader. We
+// can do that by checking whether our autoloadable classes will autoload with
+// class_exists(). If not it means there's no global autoloader in place and
+// the user is not using composer. In that case we can safely require the
+// bundled autoloader code.
+if (!\class_exists('\WPGraphQL\Extensions\Cache')) {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
+// This way we can add the vendor/ directory to git and have the plugin "just
+// work" when it is cloned to wp-content/plugins. But be careful when checking
+// the vendor/ into git so you won't add all development dependencies too. Eg.
+// before checking it in you should always run "composer install --no-dev" first.
 
-CacheManager::init();
+\WPGraphQL\Extensions\Cache\CacheManager::init();
