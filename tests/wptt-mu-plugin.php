bc4eabc1af5a14ce8abbb177ea985fa33123e446
Esa-Matti Suuronen <esa-matti@suuronen.org>
2020-05-11 10:54:30 +0300

    Skip functiontal test things in wpunit

--- tests/wptt-mu-plugin.php
+++ tests/wptt-mu-plugin.php
@@ -3,6 +3,10 @@
 use WPGraphQL\Extensions\Cache\CacheManager;
 
 add_action('plugins_loaded', function () {
+    if (!defined('WPTT_INSTALL')) {
+        return;
+    }
+
     CacheManager::register_graphql_field_cache([
         'zone' => 'functional_test',
         'query_name' => 'getPosts',
